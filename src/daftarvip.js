3850dab762a27da7a6821c38d7801fb0dc325e7a
tiagonooot <79705137+tiagonooot@users.noreply.github.com>
2021-02-26 21:10:32 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5544997462158 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO BOT:*
+_https://chat.whatsapp.com/CwYiATmt5H0GhJjcNPhTZh `
+}
+exports.daftarvip = daftarvip
